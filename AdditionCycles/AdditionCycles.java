bfb971aa113f3d3822e91cc8e6833c0821718a09
Nick Corin <nickcorin@gmail.com>
2016-01-26 18:39:51 +0200

    Topcoder solutions

--- AdditionCycles/AdditionCycles.java
+++ AdditionCycles/AdditionCycles.java
@@ -1,20 +1,4 @@
 public class AdditionCycles{
-	public static void main(String[] args){
-		int[] tests = { 26, 55, 0, 71 };
-		int[] expectedResults = { 4, 3, 1, 12 };
-
-		for( int i = 0 ; i < tests.length ; i++ ){
-			int result = cycleLength( tests[i] );
-			System.out.print(tests[i] + "\t" + expectedResults[i] + "\t" + result + "\t");
-
-			if( result == expectedResults[i] ){
-				System.out.println("PASSED");
-			} else {
-				System.out.println("FAILED");
-			}
-		}
-	}
-
 	 public static int cycleLength( int n ){
 
 	 	int cycles = 0, original = n;
--- DepositProfit/DepositProfit.java
+++
@@ -1,5 +0,0 @@
-public class DepositProfit{
-	public int depositTerm( int amount, int annualInterest, int profit ){
-		return ( int )( Math.log( amount + profit ) / Math.log( 1 + annualInterest/12 ) );
-	}
-}
